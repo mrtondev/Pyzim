54c2eeec9b1612c28bf54a6114382ff03604d08c
Ayrton Rafael <ayrton.rafael@gmail.com>
2023-04-19 21:06:11 -0300

    Triangle Type 15

--- EstruturaDeDecisão/15.0_triangleType.py
+++ EstruturaDeDecisão/15.0_triangleType.py
@@ -2,9 +2,22 @@
 #O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um 
 #triângulo, se o mesmo é: equilátero, isósceles ou escaleno. 
 
-print('¨¨¨¨¨Programa¨¨¨¨')
-print('   @@@@ de @@@')
+
+print('++++ Seja Bem-vindo ao +++++')
+print('¨¨¨¨¨ Programa ¨¨¨¨')
+print('   --- de ---')
 print(' !!!! Tipos de triângulo !!!')
+
+print('                   ') 
+print('        /\         ')
+print('       /  \        ')
+print('      /    \       ')
+print('     /      \      ')
+print('    /        \     ')
+print('   /          \    ')
+print('  /____________\   ')
+print('                   ')
+print('                   ')      
 
 s1=float(input('Digite um lado do triângulo: \n'))
 s2=float(input('Digite o segundo lado do triângulo: \n'))
@@ -13,7 +26,7 @@ s3=float(input('Agora o terceiro lado: \n'))
 if (s1 == s2) and (s2 == s3):
         print('Triângulo:')
         print('Equilátero')
-elif ((s1 == s2) and s2 != s3 and s1 != s3) or ((s1 == s3) and s2 != s1 and s2 != s3):
+elif ((s1 == s2) and s2 != s3 and s1 != s3) or ((s1 == s3) and s2 != s1 and s2 != s3) or ((s2 == s3) and s1 != s2 and s1!= s3):
         print('Triângulo')
         print('Isósceles')
 else:
--- EstruturaDeDecisão/8.0_choicePrice.py
+++ EstruturaDeDecisão/8.0_choicePrice.py
@@ -1,6 +1,7 @@
 #Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,sabendo que a decisão é sempre pelo mais barato
 
 print(' 000 Programa de economia 000')
+print('                             ')
 produto1_name=str(input('Informe o nome do primeiro produto: \n'))
 produto1_preco=float(input('Informe o valor: \n'))
 
@@ -10,13 +11,14 @@ produto2_preco=float(input('Informe o valor: \n'))
 produto3_name=str(input('Informe o nome do terceiro produto: \n'))
 produto3_preco=float(input('Informe o valor: \n'))
 
-if produto1_preco>produto2_preco>produto3_preco:
+if (produto1_preco>produto2_preco>produto3_preco) or (produto2_preco>produto1_preco>produto3_preco):
     print('Compre o ',produto3_name)
-elif produto1_preco<produto2_preco>produto3_preco:
+elif (produto1_preco<produto2_preco<produto3_preco) or (produto1_preco<produto3_preco<produto2_preco):
     print('Compre o ',produto1_name)
-elif produto1_preco<produto2_preco<produto3_preco:
-    print('Compre o ', produto1_name)
-elif produto2_preco<produto1_preco<produto3_preco:
+
+elif (produto2_preco<produto1_preco<produto3_preco) or (produto2_preco<produto3_preco<produto1_preco):
     print('Compre o ', produto2_name)
 else:
-    print('Compre o ', produto2_name)
+    print('#### ERROR ###')
+
+print('#### Fim Do Programa ####')
