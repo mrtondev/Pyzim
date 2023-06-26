6a6baf6f3dafab8fac3dbd9da984e434e687ce4c
Ayrton Rafael <ayrton.rafael@gmail.com>
2023-06-26 07:17:06 -0300

    First Def (Function)

--- EstruturaAutoDidata/_def_python.py
+++ EstruturaAutoDidata/_def_python.py
@@ -1,4 +1,27 @@
-def menu(ped):
-    {
-        
-    }
+def calculadora(numa,numb):
+
+        res=float
+        conta = str
+
+        conta = input('Operador ( + - * /) \n') 
+
+    
+        match conta:
+            case '+':
+                res = numa + numb
+                print('Resultado:{:.2f}' .format(res))   
+            case '-':
+                res = numa - numb
+                print('Resultado: {:.2f}' .format(res))
+            case '*':
+                res= numa * numb
+                print('Resultado: {:.2f}' .format(res))   
+            case '/':
+                res = numa / numb
+                print('Resultado: {:.2f}' .format(res))
+            case _:
+                print('Erro')
+
+a=float(input('Digite o valor do primeiro num \n'))
+b=float(input('Digite o valor do segundo \n'))
+calculadora(a,b)    
