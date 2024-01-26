ffeb836e945a71ca3174bbd15aa7569ea137878b
Ayrton Rafael Ribeiro de Souza <mrton.developer@gmail.com>
2024-01-26 00:50:26 -0300

    Exercício 18 Estrutura de Decisão

--- EstruturaDeDecisão/18.0_dateMounthYear.py
+++ EstruturaDeDecisão/18.0_dateMounthYear.py
@@ -3,6 +3,27 @@
 print('Programa verificador de datas: \n')
 print('\n')
 print('Digite uma data no formato dd/mm/aaaa, se for válida ou não, será informado\n')
+dd = int(input("Digite a data: \n"))
+mm = int(input("Digite o mês:  \n"))
+aaaa =int(input("Digite o ano: \n"))
 
-dd,mm,aaaa = int
 
+if dd < 31 and dd >= 1:
+    
+    if mm > 1 and  mm <= 12:
+        
+        if aaaa >= 1 :
+            print("Formato de data válido \n")
+            print("Data: ", dd,"/",mm,"/",aaaa)
+        else:
+            print('Formato de data inválido \n')
+            print('Ano Inválido ')
+            
+    else:
+        print('Formato de data inválido \n')
+        print('Mês Inválido')
+else:
+    print('Formato de data inválido \n')
+    print('Dia Inválido')
+
+     
