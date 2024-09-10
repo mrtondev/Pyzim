931618a6bf98e86bbc8d588c2a588706afd2b13f
Ayrton Rafael <mrton.developer@gmail.com>
2024-09-10 00:32:03 -0300

    Exercício 6

---
+++ EstruturaDeRepetição/6.0_upToSide.py
@@ -0,0 +1,17 @@
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+# Depois modifique o programa para que ele mostre os números um ao lado do outro. 
+x=1
+z=1
+def contadorV(x):
+    while x > 0 and x < 21:
+        print(x)
+        x+=1
+    return 
+
+contadorV(x)
+
+def contadorH(z):
+        print(z ,(z+ 1),z+2,z+3,z+4,z+5,z+6,z+7,z+8,z+9,z+10,z+11,z+12,z+13,z+14,z+15,z+16,z+17,z+18,z+19)
+        return
+
+contadorH(z)
