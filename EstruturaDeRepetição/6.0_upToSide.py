f797c5ef2df53fda9a3a21a4593ae36fad6f8d40
Ayrton Rafael <mrton.developer@gmail.com>
2024-09-10 00:55:20 -0300

    Correção 6 da preguiça

--- EstruturaDeRepetição/6.0_upToSide.py
+++ EstruturaDeRepetição/6.0_upToSide.py
@@ -1,17 +1,14 @@
 #Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
 # Depois modifique o programa para que ele mostre os números um ao lado do outro. 
-x=1
 z=1
-def contadorV(x):
-    while x > 0 and x < 21:
-        print(x)
-        x+=1
-    return 
-
-contadorV(x)
+nums=[z]
 
 def contadorH(z):
-        print(z ,(z+ 1),z+2,z+3,z+4,z+5,z+6,z+7,z+8,z+9,z+10,z+11,z+12,z+13,z+14,z+15,z+16,z+17,z+18,z+19)
+        while z > 0 and z < 20:
+             z+=1
+             print(z)
+             nums.append(z)  
         return
-
 contadorH(z)
+
+print(nums)
